7bcc4f8dea123bc3d0791b878f1ddd5d5ede6df5
elferia <a.t.qu.qu.qu+github@gmail.com>
2023-12-09 14:32:23 +0000

    timeout

--- cmd/main.go
+++ cmd/main.go
@@ -21,6 +21,10 @@ func main() {
 		return c.String(200, "Hello, World!")
 	})
 
+	e.Server.ReadTimeout = viper.GetDuration("timeout.read")
+	e.Server.WriteTimeout = viper.GetDuration("timeout.write")
+	e.Server.IdleTimeout = viper.GetDuration("timeout.keepalive")
+
 	viper.SetDefault("listen.port", "1080")
 	slog.LogAttrs(context.Background(), slog.LevelWarn, "http server stopped",
 		slog.Group("main", slog.String("error",
