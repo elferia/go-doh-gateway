f33853c1362ed22278559f403176fdcde6e45bfa
elferia <a.t.qu.qu.qu+github@gmail.com>
2023-12-09 13:20:19 +0000

    env support

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetEnvPrefix("doh_gateway")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(200, "Hello, World!")
